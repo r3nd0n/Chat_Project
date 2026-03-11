620b28fdaa3d8d1b29cc84a227e64a60cac80034
zapata <emertech.sci@gmail.com>
2026-03-11 15:08:17 -0600

    sin modificaciones

---
+++ chat/target/debug/chat.d
@@ -0,0 +1,1 @@
+/home/ahalgana/Documents/Modelado/Chat_Project/chat/target/debug/chat: /home/ahalgana/Documents/Modelado/Chat_Project/chat/src/main.rs
---
+++ chat/target/debug/deps/chat-cdd3bae76de3d51f.d
@@ -0,0 +1,5 @@
+/home/ahalgana/Documents/Modelado/Chat_Project/chat/target/debug/deps/chat-cdd3bae76de3d51f.d: src/main.rs
+
+/home/ahalgana/Documents/Modelado/Chat_Project/chat/target/debug/deps/chat-cdd3bae76de3d51f: src/main.rs
+
+src/main.rs:
